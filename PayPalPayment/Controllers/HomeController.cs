8b07785ade87b648be5ef3d924ce3ec1ee6eb993
rewqla <desares526@gmail.com>
2023-06-18 21:41:39 +0300

    Add PayPal integration

---
+++ PayPalPayment/Controllers/HomeController.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using PayPal.Api;
+using PayPalPayment.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PayPalPayment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private PayPal.Api.Payment payment;
+        private IHttpContextAccessor httpContextAccessor;
+        IConfiguration _configuration;
+        public HomeController(ILogger<HomeController> logger, IHttpContextAccessor context, IConfiguration iconfiguration)
+        {
+            _logger = logger;
+            httpContextAccessor = context;
+            _configuration = iconfiguration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PaymentWithPaypal(string Cancel = null, string blogId = "", string PayerID = "", string guid = "")
+        {
+            var ClientID = _configuration.GetValue<string>("PayPal:Key");
+            var ClientSecret = _configuration.GetValue<string>("PayPal:Secret");
+            var mode = _configuration.GetValue<string>("PayPal:mode");
+            APIContext apiContext = PaypalConfiguration.GetAPIContext(ClientID, ClientSecret, mode);
+            try
+            {
+                string payerId = PayerID;
+                if (string.IsNullOrEmpty(payerId))
+                {
+                    string baseURI = this.Request.Scheme + "://" + this.Request.Host + "/Home/PaymentWithPayPal?";
+                    var guidd = Convert.ToString((new Random()).Next(100000));
+                    guid = guidd;
+                    var createdPayment = this.CreatePayment(apiContext, baseURI + "guid=" + guid, blogId);
+                    var links = createdPayment.links.GetEnumerator();
+                    string paypalRedirectUrl = null;
+                    while (links.MoveNext())
+                    {
+                        Links lnk = links.Current;
+                        if (lnk.rel.ToLower().Trim().Equals("approval_url"))
+                        {
+                            paypalRedirectUrl = lnk.href;
+                        }
+                    }
+
+                    httpContextAccessor.HttpContext.Session.SetString("payment", createdPayment.id);
+                    return Redirect(paypalRedirectUrl);
+                }
+                else
+                {
+                    var paymentId = httpContextAccessor.HttpContext.Session.GetString("payment");
+                    var executedPayment = ExecutePayment(apiContext, payerId, paymentId as string);
+
+                    if (executedPayment.state.ToLower() != "approved")
+                    {
+                        return View("PaymentFailed");
+                    }
+                    var blogIds = executedPayment.transactions[0].item_list.items[0].sku;
+
+
+                    return View("PaymentSuccess");
+                }
+            }
+            catch (Exception)
+            {
+                return View("PaymentFailed");
+            }
+        }
+        private Payment ExecutePayment(APIContext apiContext, string payerId, string paymentId)
+        {
+            var paymentExecution = new PaymentExecution()
+            {
+                payer_id = payerId
+            };
+            this.payment = new Payment()
+            {
+                id = paymentId
+            };
+            return this.payment.Execute(apiContext, paymentExecution);
+        }
+        private Payment CreatePayment(APIContext apiContext, string redirectUrl, string blogId)
+        {
+            var itemList = new ItemList()
+            {
+                items = new List<Item>()
+            };
+            itemList.items.Add(new Item()
+            {
+                name = "Item Detail",
+                currency = "USD",
+                price = "1.00",
+                quantity = "1",
+                sku = "SKU"
+            });
+            var payer = new Payer()
+            {
+                payment_method = "paypal"
+            };
+            var redirUrls = new RedirectUrls()
+            {
+                cancel_url = redirectUrl + "&Cancel=true",
+                return_url = redirectUrl
+            };
+            //var details = new Details()
+            //{
+            //    tax = "1",
+            //    shipping = "1",
+            //    subtotal = "1"
+            //};
+            var amount = new Amount()
+            {
+                currency = "USD",
+                total = "1.00", 
+                                //details = details
+            };
+            var transactionList = new List<Transaction>();
+
+            transactionList.Add(new Transaction()
+            {
+                description = "Transaction description",
+                invoice_number = Guid.NewGuid().ToString(), //Generate an Invoice No  
+                amount = amount,
+                item_list = itemList
+            });
+            this.payment = new Payment()
+            {
+                intent = "sale",
+                payer = payer,
+                transactions = transactionList,
+                redirect_urls = redirUrls
+            };
+
+            return this.payment.Create(apiContext);
+        }
+    }
+}
---
+++ PayPalPayment/PaypalConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using PayPal.Api;
+using System.Collections.Generic;
+
+namespace PayPalPayment
+{
+    public static class PaypalConfiguration
+    {
+
+        static PaypalConfiguration()
+        {
+
+        }
+        public static Dictionary<string, string> GetConfig(string mode)
+        {
+            return new Dictionary<string, string>()
+            {
+                {"mode",mode}
+            };
+        }
+        private static string GetAccessToken(string ClientId, string ClientSecret, string mode)
+        {
+            string accessToken = new OAuthTokenCredential(ClientId, ClientSecret, new Dictionary<string, string>()
+            {
+                {"mode",mode}
+            }).GetAccessToken();
+            return accessToken;
+        }
+        public static APIContext GetAPIContext(string clientId, string clientSecret, string mode)
+        {
+            APIContext apiContext = new APIContext(GetAccessToken(clientId, clientSecret, mode));
+            apiContext.Config = GetConfig(mode);
+            return apiContext;
+        }
+    }
+}
